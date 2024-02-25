7b8866af4450328f578b06efc38fa79f27183ab7
abdelrahman_saleh <abdelrahmanisaleh19@gmail.com>
2024-02-25 19:17:33 +0200

    Finishing the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,14 @@ function sendMessage() {
     const serviceID = "service_zb4roxl";
     const templateID = "template_oly2w8k";
     const params = {
-        sendername:document.querySelector("#name").value,
-        senderemail:document.querySelector("#email").value,
-        subject:document.querySelector("#subject").value,
-        message:document.querySelector("#message").value
+        sendername: document.querySelector("#name").value,
+        senderemail: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value
     }
     emailjs.send(serviceID, templateID, params)
         .then(res => {
-            alert('thank you, ' + params["sendername"] + '! your message has been sent.');
+            alert('Thank you, ' + params["sendername"] + '! Your message has been sent.');
         })
         .catch();
 }
